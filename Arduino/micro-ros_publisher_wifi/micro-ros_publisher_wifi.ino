49fba062990e0aaf8b2e7c552ec1e74fe088d360
Henri <meyer_henri.1@web.de>
2023-04-16 13:30:44 +0200

    MPU updated

--- Arduino/micro-ros_publisher_wifi/micro-ros_publisher_wifi.ino
+++ Arduino/micro-ros_publisher_wifi/micro-ros_publisher_wifi.ino
@@ -1,5 +1,8 @@
-//sudo use docker run -it --rm --net=host microros/micro-ros-agent:foxy udp4 --port 8888 -v6 
+//sudo docker run -it --rm --net=host microros/micro-ros-agent:foxy udp4 --port 8888 -v6 
 //in a terminal to start the conection to the host pc
+
+//definitions/includes for micro_ros
+
 #include <micro_ros_arduino.h>
 
 #include <stdio.h>
@@ -25,6 +28,14 @@ rcl_node_t node;
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
+//definitions/includes for imu
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
 
 void error_loop(){
   while(1){
@@ -37,16 +48,14 @@ void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
   RCLC_UNUSED(last_call_time);
   if (timer != NULL) {
     RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-    msg.linear_acceleration.x++;
   }
 }
 
 void setup() {
+  //Wifi/ros setup
+  Wire.begin(15,14);
   Serial.begin(9600);
   set_microros_wifi_transports("FRITZ!Box 7530", "03393450089541503414", "192.168.178.25", 8888);
-  
-  
-
 
   delay(2000);
 
@@ -65,10 +74,50 @@ void setup() {
     ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Imu),
     "imu_data"));
 
-  msg.linear_acceleration.x = 0;
+
+  //imu setup
+  
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+
+  //setup motion detection
+  //mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
+  mpu.setMotionDetectionThreshold(1);
+  mpu.setMotionDetectionDuration(20);
+  mpu.setInterruptPinLatch(true);  // Keep it latched.  Will turn off when reinitialized.
+  mpu.setInterruptPinPolarity(true);
+  mpu.setMotionInterrupt(true);
+
+  Serial.println("");
+  delay(100);
 }
 
 void loop() {
+    
+    
+    //Get new sensor events with the readings
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+    msg.linear_acceleration.x=a.acceleration.x;
+    msg.linear_acceleration.y=a.acceleration.y;
+    msg.linear_acceleration.z=a.acceleration.z;
+    msg.angular_velocity.x=g.gyro.x;
+    msg.angular_velocity.y=g.gyro.y;
+    msg.angular_velocity.z=g.gyro.z;
+    
+    //publish
     RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-    msg.linear_acceleration.x++;
 }
